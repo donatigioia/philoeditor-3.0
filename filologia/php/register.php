142e06486ce8174c0bb9ebc0849a503f8b904954
donatigioia <donatigioia@gmail.com>
2016-07-01 18:07:05 +0200

    Update register.php

--- filologia/php/register.php
+++ filologia/php/register.php
@@ -1,7 +1,29 @@
 <?php
+/*	
+ File: register.php
+ Author: Gioia Donati, Fabio Vitali, Angelo di Iorio
+ Last change on: 1/07/16
+
+
+
+ Copyright (c) 2016, [Nome Cognome], DASPLab, Department of Computer Science, University of Bologna
+
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose with or without fee is hereby granted, provided that the above
+    copyright notice and this permission notice appear in all copies.
+
+    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+    SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+    OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+    CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+ */
 $file = '../data/hidden/users.json' ;
-$data=json_decode(file_get_contents("php://input")); // in data ho oggetto con campi del form 
-$users = json_decode(file_get_contents($file),true) ; // in users ho oggetto con dati database.
+$data=json_decode(file_get_contents("php://input")); 
+$users = json_decode(file_get_contents($file),true) ; 
 
 if (!$users){
 	http_response_code(501);
@@ -22,7 +44,7 @@ else{
 	$sesso=$data->gender;
 	$pwd=$data->pwd1;
 	$users[]=array("name" => $user_name, "pwd" => $pwd,"showAs"=>$nome." ".$cognome,"gender"=>$sesso);
-//	print_r($users);
+
 
 	var $ok=file_put_contents($file, json_encode($users,JSON_PRETTY_PRINT),true);		
 	if(!$ok){
@@ -41,7 +63,7 @@ else{
 
 function check_user($users, $user_name) {
 	$user_match = search_key($users, 'name', $user_name) ; 
-	if (count($user_match) != 1) {//non c'è utente
+	if (count($user_match) != 1) {
 		return 1;
 	} else {
 		return null ;
